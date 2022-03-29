f25c052481a77df5883f5584031b888cdaa8e7f7
chrisce206 <chrisce206@gmail.com>
2022-03-29 03:50:52 -0700

    restart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Header from './components/Header'
-import Projects from './components/Projects'
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   return (
-    <>
-      <Navbar />
-      <hr></hr>
-      <h1>HELLO</h1>
-      <Header />
-      <Projects />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
