30c27078079e3c59f06729ef31279790fbb6ec6f
chrisce206 <chrisce206@gmail.com>
2022-03-29 03:26:46 -0700

    me

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Projects(){
+  // const projects = {}
+
+  return(
+    <>
+      <h1 className='projects--header'>My Projects</h1>
+      <div className='projects--container'></div>
+    </>
+  )
+}
